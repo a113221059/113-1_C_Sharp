453219f615eb31d4d95aea3c09d60a7d2fa38a02
a113221059 <a113221059@mail.shu.edu.tw>
2024-10-22 18:01:29 +0800

    113-10-22

---
+++ 113-10-22/GroupBoxDemo/GroupBoxDemo/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GroupBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label2 = new Label();
+            groupBox = new GroupBox();
+            label1 = new Label();
+            groupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(248, 312);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            label2.Click += label2_Click;
+            // 
+            // groupBox
+            // 
+            groupBox.Controls.Add(label1);
+            groupBox.Controls.Add(textBox1);
+            groupBox.Location = new Point(122, 70);
+            groupBox.Name = "groupBox";
+            groupBox.Size = new Size(334, 268);
+            groupBox.TabIndex = 3;
+            groupBox.TabStop = false;
+            groupBox.Text = "個人資料";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 4;
+            label1.Text = "姓名";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox.ResumeLayout(false);
+            groupBox.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label2;
+        private GroupBox groupBox;
+        private Label label1;
+    }
+}
